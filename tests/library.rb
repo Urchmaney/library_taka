259dc6c1069deb394b7e26fde9f632e64abdfca5
Unegbu Kingsley <kingsobino@gmail.com>
2024-12-25 23:49:13 +0100

    Add test for returning book

--- tests/library.rb
+++ tests/library.rb
@@ -50,5 +50,37 @@ module LibraryTaka
         end
       end
     end
+
+    class ReturnBook < Test::Unit::TestCase
+      def setup
+        @library = Library.new
+        SAMPLE_BOOKS.each {|book| @library.add_book book }
+        @borrowed_book = SAMPLE_BOOKS[1]
+        @library.borrow_book(@borrowed_book)
+      end
+
+      def test_return_book_without_error
+        count = @library.available_books.count
+        assert_nothing_raised do
+          @library.return_book @borrowed_book
+        end
+        assert_equal @library.available_books.count, count + 1
+      end
+
+      def test_unaffected_multiple_return_book
+        count = @library.available_books.count
+        assert_nothing_raised do
+          @library.return_book @borrowed_book
+          @library.return_book @borrowed_book
+        end
+        assert_equal @library.available_books.count, count
+      end
+
+      def test_error_return_unadded_book
+        assert_raise BookNotFoundError do
+          @library.return_book Book.new("978-92-95111-00-2", "Elon Musk", "Building X", "2024")
+        end
+      end
+    end
   end
 end
